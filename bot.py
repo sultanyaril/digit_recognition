47db7d4399965e3d6b4c19308ebc306fbe5be1f9
sultanyaril <sultanyaril@gmail.com>
2021-05-18 18:16:14 +0500

    solved tech problems

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from datetime import datetime # generate log
 import random
 import os.path
 
+welcome_letter = "Здравствуйте, для построения учебной модели по обработке речи мне нужна база данных. Пожалуйста,\
+ помогите мне ее сформировать. Можете записать несколько аудио (2-3)\n"
 text_of_numbers = ["ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"]
 
 with open("creds.txt", "r") as f:
@@ -21,47 +23,54 @@ def generate_numbers():
         result += str(random.randint(0, 9)) + ' '
     return result
 
-def generate_prompt(user_name):
+def generate_prompt(user_id):
     global numbers
-    numbers[user_name] = generate_numbers()
+    numbers[user_id] = generate_numbers()
     number_text = ""
-    for i in numbers[user_name].strip().split(' '):
+    for i in numbers[user_id].strip().split(' '):
         number_text += text_of_numbers[int(i)] + ' '
     return "Пожалуйста, прочтите следующие 5 цифр, разделяя их паузой в 1 секунду\n" + number_text + "\n"
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     user_name = message.from_user.username
+    user_id = message.from_user.id
     log_text = "User ({0}): {1}".format(user_name, message.text)
     log(log_text)
-    
-    bot.send_message(message.from_user.id, generate_prompt(user_name))
+    bot.send_message(message.from_user.id, welcome_letter)   
+    bot.send_message(message.from_user.id, generate_prompt(user_id))
 
 @bot.message_handler()
 def get_text_message(message):
     user_name = message.from_user.username
+    user_id = message.from_user.id
     log_text = "User ({0}): {1}".format(user_name, message.text)
     log(log_text)
- 
+    global numbers
+    if user_id not in numbers:
+        bot.send_message(message.from_user.id, generate_prompt(user_id))
     bot.send_message(message.from_user.id, "Запишите аудио")
 
 @bot.message_handler(content_types=['voice'])
 def get_voice_message(message):
     user_name = message.from_user.username
+    user_id = message.from_user.id
     log_text = "User ({0}): {1}".format(user_name, str(message.voice))
     log(log_text)
-
+    global numbers
+    if user_id not in numbers:
+        bot.send_message(message.from_user.id, generate_prompt(user_id))
+        return
     tele_file = bot.get_file(message.voice.file_id)
     ogg_data = bot.download_file(tele_file.file_path)
-    global numbers
-    file_name = "data/" + numbers[user_name].strip().replace(' ', '_') + ".ogg"
+    file_name = "data/" + numbers[user_id].strip().replace(' ', '_') + ".ogg"
     while os.path.exists(file_name):
         file_name = file_name[:-4] + "_1" + ".ogg"
     with open(file_name, "wb") as f:
         f.write(ogg_data)
     print("FILE SAVED: ", file_name)
 
-    bot.send_message(message.from_user.id, generate_prompt(user_name))
+    bot.send_message(message.from_user.id, generate_prompt(user_id))
 
 bot.polling()
 
